1235596b0849f2780acc045a0efabdcd9a7c7ffd
Αποστολος Τσαμασλιδης <apostolostsamaslidis@Apostoloss-Mac-mini.local>
2026-04-28 15:06:53 +0300

    Fix Deepgram payload types

--- stt.py
+++ stt.py
@@ -1,16 +1,18 @@
 import os
-from deepgram import DeepgramClient, FileSource, PrerecordedOptions
+from deepgram import DeepgramClient
 
 deepgram = DeepgramClient(os.getenv("DEEPGRAM_API_KEY"))
 
 def transcribe_file(path: str) -> str:
     with open(path, "rb") as audio:
-        payload: FileSource = {"buffer": audio.read()}
+        buffer_data = audio.read()
 
-    options = PrerecordedOptions(
-        model="nova-2",
-        language="el"
-    )
+    payload = {"buffer": buffer_data}
+
+    options = {
+        "model": "nova-2",
+        "language": "el"
+    }
 
     response = deepgram.listen.rest.v("1").transcribe_file(payload, options)
-    return response.results.channels[0].alternatives[0].transcript
+    return response["results"]["channels"][0]["alternatives"][0]["transcript"]
